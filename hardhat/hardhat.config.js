6b12841cd73c9d5cf3aaabb0a2df41173d16a61d
chenronglin <chenrongli@gmail.com>
2023-08-25 08:54:17 +0000

    web

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,13 +4,13 @@ require("@nomicfoundation/hardhat-verify");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  networks: {
-    goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/Ny_NbiYPciWQIgEMAOa0ZKOdgET23Xha',
-      accounts: ['']
-    }
-  },
-  etherscan: {
-    apiKey: "Q6C4PKWES8IDXAQBPEZ72K3IV9Y93DR4EI",
-  },
+  // networks: {
+  //   goerli: {
+  //     url: 'https://eth-goerli.g.alchemy.com/v2/Ny_NbiYPciWQIgEMAOa0ZKOdgET23Xha',
+  //     accounts: ['']
+  //   }
+  // },
+  // etherscan: {
+  //   apiKey: "Q6C4PKWES8IDXAQBPEZ72K3IV9Y93DR4EI",
+  // },
 };
